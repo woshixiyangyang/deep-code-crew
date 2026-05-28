856ca66fcea0ba244e2b65f29f5a185f1d075506
Daniel <dshin6500@yonsei.ac.kr>
2026-05-29 02:25:55 +0900

    Google Sheet save/load implemented

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from controllers.party_controller import PartyController
-from services.local_storage_service import LocalStorageService
+from services.google_sheet_service import GoogleSheetService
 from views.console_view import ConsoleView
 
 # A identity: YING LAN / 2023106179 / A - Controller and core Model structure
 
 
 def main():
-    storage_service = LocalStorageService("local_storage.json")
+    storage_service = GoogleSheetService("final project")
     view = ConsoleView()
     controller = PartyController(view, storage_service)
     controller.run()
